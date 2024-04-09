b2c143b0f640feeccca1293f3cf317c17ba8aa10
piy3 <129292800+piy3@users.noreply.github.com>
2024-04-09 23:49:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let galler = document.querySelector('.gallery');
 let right = document.querySelector('.right-move');
 let left = document.querySelector('.left-move');
-
+ 
 galler.addEventListener('wheel',(event)=>{
     const delta = Math.sign(event.deltaY);
     if(delta==-1){
         galler.style.scrollBehavior = "smooth";
         galler.scrollLeft += 400;
-    }
+    }  
     else{
         galler.style.scrollBehavior = "smooth";
         galler.scrollLeft -=400;
@@ -15,21 +15,40 @@ galler.addEventListener('wheel',(event)=>{
 
 })
 
+function scroller_sub(){
+    galler.scrollLeft -=400;
+    if(galler.scrollLeft<=0){
+        galler.scrollLeft=2000;
+        
+    }
+}
+
+function scroller_add(){
+    galler.scrollLeft +=400;
+    if(galler.scrollLeft>1600){
+        galler.scrollLeft=0;
+        
+    }
+}
+
+setInterval(() => {
+    galler.style.scrollBehavior = "smooth";
+    scroller_add();
+}, 10000);
+
 
 left.addEventListener("click",(evt)=>{
-    galler.style.scrollBehavior = "smooth";
-    left.classList.add('fa-bounce')
-    galler.scrollLeft -=400;
-    setTimeout(() => {
-        
-        left.classList.remove('fa-bounce');
-    }, 500);
-})
+galler.style.scrollBehavior = "smooth";
+left.classList.add('fa-bounce')
+scroller_sub();
+setTimeout(() => {
+    left.classList.remove('fa-bounce');
+}, 500);})
 
 right.addEventListener("click",()=>{
     galler.style.scrollBehavior = "smooth";
     right.classList.add('fa-bounce')
-    galler.scrollLeft += 400;
+    scroller_add();
     setTimeout(() => {
         right.classList.remove("fa-bounce")
     }, 500);
